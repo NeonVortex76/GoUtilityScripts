0e3a311d65805c16e1b0bdc56d34fb8a5134441e
NeonVortex76 <carrots.cosy.0u@icloud.com>
2024-10-12 16:54:06 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-func removeDuplicateOperations() {
+func sortOperationsByDate() {
     inputFile, err := os.Open("results.txt")
     if err != nil {
         log.Println("Error opening file:", err)
@@ -6,26 +6,44 @@ func removeDuplicateOperations() {
     }
     defer inputFile.Close()
 
-    tempFile, err := os.Create("temp_results.txt")
+    var operations []string
+    scanner := bufio.NewScanner(inputFile)
+
+    for scanner.Scan() {
+        operations = append(operations, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Println("Error reading file:", err)
+        return
+    }
+
+    sort.Slice(operations, func(i, j int) bool {
+        layout := "2006-01-02" // Формат даты, например, 2024-10-03
+        dateI := strings.Split(operations[i], " ")[0]
+        dateJ := strings.Split(operations[j], " ")[0]
+        timeI, errI := time.Parse(layout, dateI)
+        timeJ, errJ := time.Parse(layout, dateJ)
+
+        if errI != nil || errJ != nil {
+            log.Println("Error parsing date:", errI, errJ)
+            return false
+        }
+        return timeI.Before(timeJ)
+    })
+
+    tempFile, err := os.Create("sorted_results.txt")
     if err != nil {
         log.Println("Error creating temp file:", err)
         return
     }
     defer tempFile.Close()
 
-    scanner := bufio.NewScanner(inputFile)
     writer := bufio.NewWriter(tempFile)
-
-    operationsSet := make(map[string]bool)
-
-    for scanner.Scan() {
-        line := scanner.Text()
-        if _, exists := operationsSet[line]; !exists {
-            operationsSet[line] = true
-            _, err := writer.WriteString(line + "\n")
-            if err != nil {
-                log.Println("Error writing to temp file:", err)
-            }
+    for _, operation := range operations {
+        _, err := writer.WriteString(operation + "\n")
+        if err != nil {
+            log.Println("Error writing to temp file:", err)
         }
     }
 
@@ -35,7 +53,7 @@ func removeDuplicateOperations() {
     }
 
     os.Remove("results.txt")
-    os.Rename("temp_results.txt", "results.txt")
+    os.Rename("sorted_results.txt", "results.txt")
 
-    fmt.Println("Duplicate operations have been removed.")
+    fmt.Println("Operations sorted by date.")
 }
