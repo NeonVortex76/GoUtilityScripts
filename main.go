724ebe70de15b6e6c6a8375b311a9bef0d3079da
NeonVortex76 <carrots.cosy.0u@icloud.com>
2024-10-17 22:09:25 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-func countOperationsByKeyword() {
+func removeDuplicateOperations() {
     inputFile, err := os.Open("results.txt")
     if err != nil {
         log.Println("Error opening file:", err)
@@ -6,23 +6,45 @@ func countOperationsByKeyword() {
     }
     defer inputFile.Close()
 
-    var keyword string
-    fmt.Print("Enter the keyword to count operations by: ")
-    fmt.Scanln(&keyword)
-
+    operationsMap := make(map[string]bool)
     scanner := bufio.NewScanner(inputFile)
-    count := 0
+    uniqueOperations := []string{}
 
     for scanner.Scan() {
         line := scanner.Text()
-        if strings.Contains(line, keyword) {
-            count++
+        if !operationsMap[line] {
+            operationsMap[line] = true
+            uniqueOperations = append(uniqueOperations, line)
         }
     }
 
     if err := scanner.Err(); err != nil {
         log.Println("Error reading file:", err)
+        return
     }
 
-    fmt.Printf("Total operations containing '%s': %d\n", keyword, count)
+    tempFile, err := os.Create("temp_results.txt")
+    if err != nil {
+        log.Println("Error creating temp file:", err)
+        return
+    }
+    defer tempFile.Close()
+
+    writer := bufio.NewWriter(tempFile)
+    for _, operation := range uniqueOperations {
+        _, err := writer.WriteString(operation + "\n")
+        if err != nil {
+            log.Println("Error writing to temp file:", err)
+        }
+    }
+
+    err = writer.Flush()
+    if err != nil {
+        log.Println("Error flushing to temp file:", err)
+    }
+
+    os.Remove("results.txt")
+    os.Rename("temp_results.txt", "results.txt")
+
+    fmt.Println("Duplicate operations removed.")
 }
