4d41642d7118cc87f6ec15d8b295f8365cd5bfa3
NeonVortex76 <carrots.cosy.0u@icloud.com>
2024-10-27 01:26:14 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-func filterOperationsByKeywordCount() {
+func renameKeywordInOperations() {
     inputFile, err := os.Open("results.txt")
     if err != nil {
         log.Println("Error opening file:", err)
@@ -6,32 +6,38 @@ func filterOperationsByKeywordCount() {
     }
     defer inputFile.Close()
 
-    var keyword string
-    var minCount, maxCount int
-    fmt.Print("Enter the keyword to filter by: ")
-    fmt.Scanln(&keyword)
-    fmt.Print("Enter minimum count: ")
-    fmt.Scanln(&minCount)
-    fmt.Print("Enter maximum count: ")
-    fmt.Scanln(&maxCount)
+    var oldKeyword, newKeyword string
+    fmt.Print("Enter the keyword to rename: ")
+    fmt.Scanln(&oldKeyword)
+    fmt.Print("Enter the new keyword: ")
+    fmt.Scanln(&newKeyword)
 
-    operationCounts := make(map[string]int)
+    tempFile, err := os.Create("temp_results.txt")
+    if err != nil {
+        log.Println("Error creating temp file:", err)
+        return
+    }
+    defer tempFile.Close()
+
     scanner := bufio.NewScanner(inputFile)
+    writer := bufio.NewWriter(tempFile)
 
     for scanner.Scan() {
         line := scanner.Text()
-        if strings.Contains(line, keyword) {
-            operationCounts[line]++
+        modifiedLine := strings.ReplaceAll(line, oldKeyword, newKeyword)
+        _, err := writer.WriteString(modifiedLine + "\n")
+        if err != nil {
+            log.Println("Error writing to temp file:", err)
         }
     }
 
-    for operation, count := range operationCounts {
-        if count >= minCount && count <= maxCount {
-            fmt.Printf("%s - Count: %d\n", operation, count)
-        }
+    err = writer.Flush()
+    if err != nil {
+        log.Println("Error flushing to temp file:", err)
     }
 
-    if err := scanner.Err(); err != nil {
-        log.Println("Error reading file:", err)
-    }
+    os.Remove("results.txt")
+    os.Rename("temp_results.txt", "results.txt")
+
+    fmt.Println("Keyword renamed in operations.")
 }
