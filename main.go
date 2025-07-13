8e5b2f7a0246a06186a558ff3114572cedb20375
NeonVortex76 <carrots.cosy.0u@icloud.com>
2025-07-13 19:28:17 +0200

    refactor: extract line duplication logic into helper function

--- main.go
+++ main.go
@@ -14,21 +14,9 @@ func duplicateLines(lineNumber int) {
     defer tempFile.Close()
 
     scanner := bufio.NewScanner(inputFile)
-    currentLine := 1
-    for scanner.Scan() {
+    for currentLine := 1; scanner.Scan(); currentLine++ {
         line := scanner.Text()
-        _, err := tempFile.WriteString(line + "\n")
-        if err != nil {
-            log.Println("Error writing to temporary file:", err)
-        }
-
-        if currentLine == lineNumber {
-            _, err := tempFile.WriteString(line + "\n")
-            if err != nil {
-                log.Println("Error duplicating line in temporary file:", err)
-            }
-        }
-        currentLine++
+        writeLineTwiceIfNeeded(tempFile, line, currentLine == lineNumber)
     }
 
     if err := scanner.Err(); err != nil {
@@ -36,10 +24,16 @@ func duplicateLines(lineNumber int) {
         return
     }
 
-    err = os.Rename("results_temp.txt", "results.txt")
-    if err != nil {
+    if err := os.Rename("results_temp.txt", "results.txt"); err != nil {
         log.Println("Error replacing original file:", err)
     }
 
     fmt.Printf("Duplicated line %d in the file.\n", lineNumber)
+}
+
+func writeLineTwiceIfNeeded(f *os.File, line string, duplicate bool) {
+    f.WriteString(line + "\n")
+    if duplicate {
+        f.WriteString(line + "\n")
+    }
 }
